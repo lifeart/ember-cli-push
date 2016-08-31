69b4808011190ff1f0095256158581001caed2a3
lifeart <lifeart92@gmail.com>
2016-08-31 04:54:45 +0300

    verion bump * blueprint fix

--- blueprints/ember-cli-push/index.js
+++ blueprints/ember-cli-push/index.js
@@ -1,15 +1,14 @@
 /*jshint node:true*/
 module.exports = {
-  description: '',
-
   // locals: function(options) {
   //   // Return custom template variables here.
   //   return {
   //     foo: options.entity.options.foo
   //   };
   // }
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
-   afterInstall: function(options) {
+  afterInstall: function(options) {
      return this.addBowerPackageToProject('push.js');
   }
 };
